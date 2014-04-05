961d1f44dcc5e28990c9c1154859b30a0700bb08
Philipp Gampe <philipp.gampe@typo3.org>
2014-04-05 18:06:44 +0200

    [FEATURE] Add converter

--- Classes/Controller/ConverterController.php
+++ Classes/Controller/ConverterController.php
@@ -6,6 +6,7 @@ namespace Tx\Newstocontentrelation\Controller;
  *
  *  (c) 2014 Philipp Gampe <philipp.gampe@typo3.org>
  *  All rights reserved
+ *
  *  This script is part of the TYPO3 project. The TYPO3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -42,31 +43,68 @@ class ConverterController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
 	 * @param integer $next The amount of news entries to process
 	 * @return void
 	 */
-	public function indexAction($current, $next) {
+	public function indexAction($current = 0, $next = 10) {
 		$this->view->assign('current', (integer)$current);
 		$this->view->assign('next', (integer)$next);
 	}
 
 	/**
-	 * Convert the records
+	 * Convert the next round of records
 	 *
-	 * @param integer $start
-	 * @param integer $count
+	 * @param integer $current The current position
+	 * @param integer $next The amount of records to convert
+	 * @return void
 	 */
-	protected function convertAction($start = 0, $count = 10) {
-
+	protected function convertAction($current = 0, $next = 10) {
+		$last = FALSE;
+		$count = $this->getDatabaseConnection()->exec_SELECTcountRows('uid', 'tx_news_domain_model_news');
+		$end = $current + $next;
+		if ($end > $count) {
+			$end = $count;
+			$last = TRUE;
+		}
+		$this->process($current, $end);
+		if (!$last) {
+			$this->redirect('index', NULL, NULL, array('current' => $end + 1, 'next' => $next));
+		}
+		$this->view->assign('total', $end);
 	}
 
 	/**
 	 * Does the actual conversion
 	 *
 	 * @param integer $current The current position
-	 * @param integer $count The amount of records to convert
-	 * @return mixed
+	 * @param integer $end The position of the last converted record
+	 * @return void
 	 */
-	protected function process($current, $count) {
-
-		return $current;
+	protected function process($current, $end) {
+		for ($recordUid = $current; $recordUid <= $end; $recordUid++) {
+			/** @var \tx_news_domain_model_news $news */
+			$news = $this->newsRepository->findByUid($recordUid);
+			if ($news === NULL) {
+				continue;
+			}
+			$text = trim($news->getBodytext());
+			if ($text !== '') {
+				$news->setBodytext('');
+				/** @var \Tx_News_Domain_Model_TtContent $content */
+				$content = $this->objectManager->get('Tx_News_Domain_Model_TtContent');
+				$content->setBodytext($text);
+				$content->setCType('text');
+				$content->setPid($news->getPid());
+				$content->setHeaderLayout(100);
+				$news->addContentElement($content);
+				$this->newsRepository->update($news);
+			}
+		}
 	}
 
+	/**
+	 * Gets the database connection
+	 *
+	 * @return \TYPO3\CMS\Core\Database\DatabaseConnection
+	 */
+	protected function getDatabaseConnection() {
+		return $GLOBALS['TYPO3_DB'];
+	}
 } 
